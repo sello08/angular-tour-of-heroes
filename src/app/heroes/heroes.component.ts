30c34203913c0fadc44dbfb9eb6a027fa757cde8
Selim Akyol <selimakyol25@gmail.com>
2022-10-03 20:33:47 +0300

    buttons created

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,25 @@
+
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero.interface';
+import { Heroes } from './../hero.container';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+
+  selectedHero? : Hero;
+  public heroes = Heroes;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+}
